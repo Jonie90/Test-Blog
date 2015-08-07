d7e0ca59a9e416cf2b5220861e50f8510c1beb85
Karolin Jonsson <KarroJ@Karolins-MacBook-Pro.local>
2015-08-07 11:49:37 +0200

    adding views and stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_devise_params, if: :devise_controller?
+  #If we are in the edvise controller, override wirg configure_devise_aprams
+
+  private
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :name)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :name, :current_password)
+    end
+  end
 end
